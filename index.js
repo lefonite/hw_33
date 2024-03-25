1b858eb2ccc0a14c42ab775cc53dcd469162a68f
le-fonite <lefonite@gmail.com>
2024-03-26 01:58:28 +0200

    Ініціалізація проекту

---
+++ index.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(sum(5, 3)); // 8
+console.log(subtract(10, 4)); // 6
+console.log(multiply(2, 7)); // 14
